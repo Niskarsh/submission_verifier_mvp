cc72c805d42b57d2d3d5f1fbe8186f580e72d569
Niskarsh <niskarshk@gmail.com>
2025-02-23 22:23:55 +0530

    updated prompt

--- src/components/ChatSection.js
+++ src/components/ChatSection.js
@@ -32,7 +32,7 @@ function ChatSection({ figmaFile, selectedSkillsets }) {
   const initialPrompt = `
 You are an automated verifier tasked with assessing a submitter's claimed skillsets based on their provided submission, which may include links (e.g., to a Figma file) and other resources. Given a specific submission and a list of skillsets to test, your responsibilities are as follows:
 
-1. Generate a series of tasks that the submitter must complete. Each task should be directly related to the submission and designed to thoroughly evaluate the corresponding claimed skillsets.
+1. Generate a series of actionable tasks that the submitter must complete. These task are supposed to be minor updates on existing figma file that tests the targeted skillset. Each task should be directly related to the submission and designed to thoroughly evaluate the corresponding claimed skillsets.
 2. Ensure that the combined set of tasks comprehensively covers all the claimed skillsets.
 3. Maintain the overall conversation context even if the submitter engages in additional dialogue between tasks.
 
